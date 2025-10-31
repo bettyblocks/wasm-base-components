2c77ced325c4202a856644b0f6d6d27661bf3713
Thomas Timmer <thomas.timmer@bettyblocks.com>
2025-10-31 10:49:28 +0100

    fix: add two generate github secrets bun scripts

---
+++ deploy/keyvault-list-generate.js
@@ -0,0 +1,35 @@
+import { $ } from "bun";
+
+const BETTY_PREFIX = "betty-";
+
+async function getKeyvaults(accountId) {
+  const keyvaults = await $`az keyvault list --subscription ${accountId}`.json();
+  return keyvaults.map(keyvault => keyvault.name).filter(name => name.startsWith(BETTY_PREFIX));
+}
+
+async function getKeyvaultInfo(accountId, keyvault) {
+  let keyvaultInfo = await $`az keyvault show --subscription ${accountId} --name ${keyvault}`.json();
+  const zone = keyvault.split("-")[1];
+  return [zone, keyvaultInfo.properties.vaultUri];
+}
+
+const accounts = await $`az account list`.json();
+const accountIds = accounts.map(account => account.id);
+const keyvaultsWithAccountId = {};
+const tasks = [];
+for (const accountId of accountIds) {
+  tasks.push(getKeyvaults(accountId).then(keyvaults => {
+    keyvaults.forEach(keyvault => {
+      keyvaultsWithAccountId[keyvault] = accountId;
+    });
+  }));
+}
+await Promise.all(tasks);
+
+const tasks2 = [];
+for (const [keyvault, accountId] of Object.entries(keyvaultsWithAccountId)) {
+  tasks2.push(getKeyvaultInfo(accountId, keyvault))
+}
+let results = await Promise.all(tasks2)
+const zoneLookup = Object.fromEntries(results.filter(([key]) => key !== "backup").toSorted());
+console.log(zoneLookup);
---
+++ deploy/keyvault-secrets-generate.js
@@ -0,0 +1,38 @@
+import { $ } from "bun";
+
+const BETTY_PREFIX = "betty-";
+
+async function getKeyvaults(accountId) {
+  const keyvaults = await $`az keyvault list --subscription ${accountId}`.json();
+  return keyvaults.map(keyvault => keyvault.name).filter(name => name.startsWith(BETTY_PREFIX));
+}
+
+async function getKeyvaultSecrets(accountId, keyvault) {
+  if (keyvault.includes("backup")) {
+    return [];
+  }
+  const secrets = await $`az keyvault secret show --name actions-compiler-secrets --subscription ${accountId} --vault-name ${keyvault}`.json();
+  const zone = keyvault.split("-")[1];
+  return [zone, JSON.parse(secrets.value)["ACTIONS_COMPILER_GITHUB_SECRET"]];
+}
+
+const accounts = await $`az account list`.json();
+const accountIds = accounts.map(account => account.id);
+const keyvaultsWithAccountId = {};
+const tasks = [];
+for (const accountId of accountIds) {
+  tasks.push(getKeyvaults(accountId).then(keyvaults => {
+    keyvaults.forEach(keyvault => {
+      keyvaultsWithAccountId[keyvault] = accountId;
+    });
+  }));
+}
+await Promise.all(tasks);
+
+const tasks2 = [];
+for (const [keyvault, accountId] of Object.entries(keyvaultsWithAccountId)) {
+  tasks2.push(getKeyvaultSecrets(accountId, keyvault))
+}
+let results = await Promise.all(tasks2)
+const secretsLookup = Object.fromEntries(results.filter(([key]) => key).toSorted());
+console.log(secretsLookup);
