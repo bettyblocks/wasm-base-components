c6b64ace7423604dc5791e9ea0df71020c69e753
Dewinz <day@gopi.day>
2026-06-03 17:06:18 +0200

    doc: remove unnecessary TODO and add extra explanation for getting the reserved ID based on the negative ID

--- helper/data-api/src/context.rs
+++ helper/data-api/src/context.rs
@@ -313,9 +313,9 @@ fn get_reserved_id_as_value_for_negative_id(
     reserved_ids: &[usize],
     negative_id: i64,
 ) -> serde_json::Value {
-    // TODO: Do we need guardrails around this to make sure the value is positive and error
-    // otherwise, or just trust it works?
     serde_json::Value::Number(serde_json::Number::from(
+        // If `negative_id` is -1 which is the first possible negative ID, then `-1 - -1 = 0`.
+        // Which is the first reserved ID.
         reserved_ids[(-1 - negative_id) as usize],
     ))
 }
